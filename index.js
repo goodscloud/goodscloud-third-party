ce1b72f3d3b2bc3c96535d7da0d87f12bf3a1346
Giampaolo Rodola <g.rodola@gmail.com>
2014-09-29 17:31:42 +0200

    interact with gc API

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var express = require('express')
 var app = express();
+var gclib = require('goodscloud');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    var c = new gclib('http://sandbox.goodscloud.com');
+    c.login('XXX', 'XXX',
+        function () {
+            console.info("Logged in as", c.email);
+            c.get('/api/internal/company', {flat: true}, function (data) {
+                console.info(data);
+            });
+        }
+    );
+
+    response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
