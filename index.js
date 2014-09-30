694f9eb92750fe621443c3d42e32af9c3d3234c0
Florian Ilgenfritz <florian@goodscloud.net>
2014-09-30 14:35:41 +0200

    basic app getting orders from last 24 hours

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 var gclib = require('goodscloud');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var gc = new gclib(process.env.GC_HOST || 'http://sandbox.goodscloud.com');
 
-app.get('/', function(request, response) {
-    var c = new gclib('http://sandbox.goodscloud.com');
-    c.login('XXX', 'XXX',
-        function () {
-            console.info("Logged in as", c.email);
-            c.get('/api/internal/company', {flat: true}, function (data) {
-                console.info(data);
-            });
-        }
-    );
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-    response.send('Hello World!')
-})
+function getDate(hours_before) {
+  var current_date = new Date();
+  return new Date(current_date.setHours(current_date.getHours() - hours_before)).toISOString();
+}
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get('/', function (request, response) {
+  var username, password;
+  username = process.env.GC_USERNAME;
+  password = process.env.GC_PASSWORD;
+  gc.login(username, password, function() {
+    gc.get('/api/internal/order', {q: {filters: [
+      {name: 'placed', op: '>=', val: getDate(24)}
+    ]}}, function (data) {
+      response.send(data);
+    });
+  });
+});
+
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
