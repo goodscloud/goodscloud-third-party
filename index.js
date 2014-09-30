e7b21ae877335b888fbed7b92c0bacce4b033422
Florian Ilgenfritz <florian@goodscloud.net>
2014-09-30 17:29:27 +0200

    limiting and formatting data shown in the web view

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var express = require('express');
 var app = express();
 var gclib = require('goodscloud');
@@ -9,18 +10,29 @@ app.use(express.static(__dirname + '/public'));
 
 function getDate(hours_before) {
   var current_date = new Date();
-  return new Date(current_date.setHours(current_date.getHours() - hours_before)).toISOString();
+  return new Date(current_date.setHours(current_date.getHours() - hours_before));
 }
 
 app.get('/', function (request, response) {
-  var username, password;
+
+  var username, password, date, num_results, result;
+
   username = process.env.GC_USERNAME;
   password = process.env.GC_PASSWORD;
+  date = getDate(24);
+
   gc.login(username, password, function() {
     gc.get('/api/internal/order', {q: {filters: [
-      {name: 'placed', op: '>=', val: getDate(24)}
+      {name: 'placed', op: '>=', val: date.toISOString()}
     ]}}, function (data) {
-      response.send(data);
+      num_results = data['num_results'];
+      if (num_results != 0) {
+        result = num_results + ' new orders since ' + date + ': ' +
+          _.pluck(data['objects'], 'external_identifier').join(', ');
+      } else {
+        result = 'No new results since ' + date + '.';
+      }
+      response.send(result);
     });
   });
 });
